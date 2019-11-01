ae99e3f49160a4b4cc5dae1dae369103d8688d14
seanpohyx <seanpohyx@gmail.com>
2019-11-02 00:54:35 +0800

    internet checker

--- source code/Kinderfind/app/src/main/java/com/example/kinderfind/adapters/FirebaseSuccessListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.kinderfind.adapters;
-
-public interface FirebaseSuccessListener {
-    //this is to listen for kindergarten data
-    void onKindergartenDataCompleted(boolean isDataCompleted);
-}
